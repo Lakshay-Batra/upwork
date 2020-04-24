6e85f7f0a7cc1de3b5f2b2bae727e14ac9421af5
Demo-Lakshay <50356418+Lakshay-Batra@users.noreply.github.com>
2020-04-25 04:41:23 +0530

    update create-your-profile

--- create-your-profile.js
+++ create-your-profile.js
@@ -1,3 +1,9 @@
 $(function () {
     $("#datepicker").datepicker();
 });
+$("#save-btn-past-experience").on("click", () => {
+    const company = $("#company").val();
+    const title = $("#past-title").val();
+    $("#past-experience").append("<h6><b>Company</b>: "+company+" | <b>Title:</b> "+title+"</h6>");
+    $('#myModal').modal('hide')
+});
