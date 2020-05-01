7545c53d750822053cfb1ce18e46a0d62fb47da7
debarshi <debopiku1122@gmail.com>
2020-05-01 21:10:41 +0530

    files modified

--- index.php
+++ index.php
@@ -41,7 +41,11 @@ if(isset($_POST['signin']))
 	{
 		$_SESSION['firstname'] = $db_firstname;
 		$_SESSION['id'] = $db_id;
+<<<<<<< HEAD
 		header("Location: create.php");
+=======
+		header("Location: create-your-profile.php");
+>>>>>>> master
 	}
 	else
 	{
@@ -71,6 +75,15 @@ if(isset($_POST['signin']))
 	<link href="style.css" rel="stylesheet">
 
 	<script src="https://kit.fontawesome.com/18dd5346aa.js" crossorigin="anonymous"></script>
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+	<script lang="javascript" type="text/javascript">
+	window.history.forward();
+	</script>
+=======
+>>>>>>> backend
+>>>>>>> master
 
 </head>
 
@@ -140,6 +153,19 @@ if(isset($_POST['signin']))
 		<h6>Upwork expertly connects professionals and agencies to businesses seeking specialized talent.</h6>
 	</div>
 
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<style>
+	.card-body p{
+		color: black;
+	}
+</style>
+	<!-- <hr class="green"> -->
+	<div class="container-fluid body-section" style="padding-top: 4%;">
+	<?php //echo $count; ?>
+=======
+>>>>>>> master
 	<style>
 		.card-body p {
 			color: black;
@@ -148,6 +174,10 @@ if(isset($_POST['signin']))
 	<!-- <hr class="green"> -->
 	<div class="container-fluid body-section" style="padding-top: 4%;">
 		<?php //echo $count; ?>
+<<<<<<< HEAD
+=======
+>>>>>>> backend
+>>>>>>> master
 		<h1 style="color: teal; text-shadow: 1px 1px 2px grey; line-height: 2;">Find quality talent and agencies</h1>
 		<div class="category-container row">
 			<div class="category-card col-md-3">
@@ -165,8 +195,18 @@ if(isset($_POST['signin']))
 				<div class="card">
 					<img src="images/Writing.png" class="card-img-top" alt="...">
 					<div class="card-body">
+<<<<<<< HEAD
 						<a href="register.php">
 							<p>Writing</p>
+=======
+<<<<<<< HEAD
+					<a href="register.php">
+						<p>Writing</p>
+=======
+						<a href="register.php">
+							<p>Writing</p>
+>>>>>>> backend
+>>>>>>> master
 						</a>
 					</div>
 				</div>
@@ -175,8 +215,18 @@ if(isset($_POST['signin']))
 				<div class="card">
 					<img src="images/Sales&Marketing.png" class="card-img-top" alt="...">
 					<div class="card-body">
+<<<<<<< HEAD
 						<a href="register.php">
 							<p>Sale & Marketing</p>
+=======
+<<<<<<< HEAD
+					<a href="register.php">
+						<p>Sale & Marketing</p>
+=======
+						<a href="register.php">
+							<p>Sale & Marketing</p>
+>>>>>>> backend
+>>>>>>> master
 						</a>
 					</div>
 				</div>
@@ -185,8 +235,18 @@ if(isset($_POST['signin']))
 				<div class="card">
 					<img src="images/Engineering&Architecture.png" class="card-img-top" alt="...">
 					<div class="card-body">
+<<<<<<< HEAD
 						<a href="register.php">
 							<p>Engineering & Architecture</p>
+=======
+<<<<<<< HEAD
+					<a href="register.php">
+						<p>Engineering & Architecture</p>
+=======
+						<a href="register.php">
+							<p>Engineering & Architecture</p>
+>>>>>>> backend
+>>>>>>> master
 						</a>
 					</div>
 				</div>
@@ -197,7 +257,15 @@ if(isset($_POST['signin']))
 				<div class="card">
 					<img src="images/Design&Creative.png" class="card-img-top" alt="...">
 					<div class="card-body"><a href="register.php">
+<<<<<<< HEAD
 							<p>Design & Creative</p>
+=======
+<<<<<<< HEAD
+						<p>Design & Creative</p>
+=======
+							<p>Design & Creative</p>
+>>>>>>> backend
+>>>>>>> master
 						</a>
 					</div>
 				</div>
@@ -206,7 +274,15 @@ if(isset($_POST['signin']))
 				<div class="card">
 					<img src="images/DataScience&Analytics.png" class="card-img-top" alt="...">
 					<div class="card-body"><a href="register.php">
+<<<<<<< HEAD
 							<p>DataScience & Analytics</p>
+=======
+<<<<<<< HEAD
+						<p>DataScience & Analytics</p>
+=======
+							<p>DataScience & Analytics</p>
+>>>>>>> backend
+>>>>>>> master
 						</a>
 					</div>
 				</div>
@@ -215,7 +291,15 @@ if(isset($_POST['signin']))
 				<div class="card">
 					<img src="images/CustomerService.png" class="card-img-top" alt="...">
 					<div class="card-body"><a href="register.php">
+<<<<<<< HEAD
 							<p>Customer Service</p>
+=======
+<<<<<<< HEAD
+						<p>Customer Service</p>
+=======
+							<p>Customer Service</p>
+>>>>>>> backend
+>>>>>>> master
 						</a>
 					</div>
 				</div>
@@ -224,7 +308,15 @@ if(isset($_POST['signin']))
 				<div class="card">
 					<img src="images/Admin Support.png" class="card-img-top" alt="...">
 					<div class="card-body"><a href="register.php">
+<<<<<<< HEAD
 							<p>Admin Support</p>
+=======
+<<<<<<< HEAD
+						<p>Admin Support</p>
+=======
+							<p>Admin Support</p>
+>>>>>>> backend
+>>>>>>> master
 						</a>
 					</div>
 				</div>
@@ -390,9 +482,18 @@ if(isset($_POST['signin']))
 		</div>
 	</div>
 
+<<<<<<< HEAD
 	<script lang="javascript" type="text/javascript">
 		window.history.forward();
 	</script>
+=======
+<<<<<<< HEAD
+=======
+	<script lang="javascript" type="text/javascript">
+		window.history.forward();
+	</script>
+>>>>>>> backend
+>>>>>>> master
 
 	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
